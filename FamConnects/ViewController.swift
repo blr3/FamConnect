333e0d14d1df4ede3e4825c27d342ff8d944d42d
Brianna <blr3@illinois.edu>
2020-07-29 23:05:36 -0500

    Initial Commit

---
+++ FamConnects/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FamConnects
+//
+//  Created by Brianna R on 7/29/20.
+//  Copyright © 2020 Brianna Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
